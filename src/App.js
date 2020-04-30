85adba7dad7bd4c7c48d7e7bc6a3435de0d436c7
Bill Kamberos <bill.kamberos@gmail.com>
2020-04-30 00:08:01 -0400

    Added JSON for images/clicked status

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import Grid from "./components/Grid";
 // import Score from "./components/Score";
 import Wrapper from "./components/Wrapper";
+import images from "./images.json"
 import './App.css';
 
 function App() {
