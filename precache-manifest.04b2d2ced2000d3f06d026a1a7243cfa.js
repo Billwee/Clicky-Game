2b6595b4dbb27a27c2be56c39fc394089a63892c
Bill Kamberos <bill.kamberos@gmail.com>
2020-05-04 20:28:26 -0400

    Updates

---
+++ precache-manifest.04b2d2ced2000d3f06d026a1a7243cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9ab265f055312e88267043fcd0cfb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26eee7872a9ee9610dce",
+    "url": "./static/css/main.74a304de.chunk.css"
+  },
+  {
+    "revision": "7dd2cf93bd7f242f4225",
+    "url": "./static/js/2.fb52bb27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fb52bb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26eee7872a9ee9610dce",
+    "url": "./static/js/main.ae55023b.chunk.js"
+  },
+  {
+    "revision": "50f98006a76af029f8ec",
+    "url": "./static/js/runtime-main.3ee45e31.js"
+  }
+]);
