6d0f04dda8e0229a3cfda5445201740c052bc196
Bill Kamberos <bill.kamberos@gmail.com>
2020-05-05 00:02:10 -0400

    Fixed path for bg image

--- src/components/Jumbotron/index.jsx
+++ src/components/Jumbotron/index.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 function Jumbotron(props) {
   return (
     <div className="jumbotron mt-4">
-      <img id="logo" src="/img/logo.png" alt="" />
+      <img id="logo" src="./img/logo.png" alt="" />
       <h3 id="clickText">Clicky Game</h3>
       <h5 className="">Don't click the same image twice!</h5>
       {props.children}
